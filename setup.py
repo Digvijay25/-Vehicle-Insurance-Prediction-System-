b3920ef7ca32ced468e388cd7ce95184a82496b6
Digvijay Singh <digvijay2560@gmail.com>
2024-12-25 17:39:47 +0000

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Digvijay Singh",
+    author_email="digvijay2560@gmail.com",
+    packages=find_packages()
+)
